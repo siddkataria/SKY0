650734c2cb022b7b1fbfb5daaaeaa06f5612c030
Sidd Kataria <katariasiddhesh1@gmail.com>
2020-10-04 17:30:29 +0530

    init

---
+++ SKY0/js/main.js
@@ -0,0 +1,280 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+	/* --------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+  	})
+
+
+  	/*---------------------------------------------------- */
+	/* FitVids
+	------------------------------------------------------ */ 
+  	$(".fluid-video-wrapper").fitVids();
+
+
+	/* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+	$(".home-slides").vegas({
+		transition: 'fade',
+		transitionDuration: 2500,
+		delay: 5000,
+    	slides: [
+       	{ src: "images/slides/03.jpg" },
+        	{ src: "images/slides/02.jpg" },
+        	{ src: "images/slides/01.jpg" }
+    	]
+	});
+
+
+	/* --------------------------------------------------- */
+	/*  Particle JS
+	------------------------------------------------------ */
+	$('.home-particles').particleground({
+	   dotColor: '#fff',
+	   lineColor: '#555555',
+	   particleRadius: 6,
+	   curveLines: true,
+	   density: 10000,
+	   proximity: 110
+	}); 
+
+
+	/*-----------------------------------------------------*/
+	/* tabs
+  	-------------------------------------------------------*/	
+	$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("ul.tabs li").click(function () {
+	   $("ul.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});
+
+
+	/*----------------------------------------------------*/
+  	/* Smooth Scrolling
+  	------------------------------------------------------*/
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+
+
+  	/* --------------------------------------------------- */
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()  
+
+
+  	/*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
+	});
+
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'We have sent you a confirmation email',
+	//  1: 'Please enter a value',
+	//  2: 'An email address must contain a single @',
+	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+	$.ajaxChimp.translations.es = {
+	  'submit': 'Submitting...',
+	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+	}
+
+
+	/*---------------------------------------------------- */
+	/*	contact form
+	------------------------------------------------------ */
+
+	/* local validation */
+	$('#contactForm').validate({
+
+		/* submit via ajax */
+		submitHandler: function(form) {
+
+			var sLoader = $('#submit-loader');
+
+			$.ajax({      	
+
+		      type: "POST",
+		      url: "inc/sendEmail.php",
+		      data: $(form).serialize(),
+		      beforeSend: function() { 
+
+		      	sLoader.fadeIn(); 
+
+		      },
+		      success: function(msg) {
+
+	            // Message was sent
+	            if (msg == 'OK') {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').hide();
+	               $('#contactForm').fadeOut();
+	               $('#message-success').fadeIn();   
+	            }
+	            // There was an error
+	            else {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').html(msg);
+		            $('#message-warning').fadeIn();
+	            }
+
+		      },
+		      error: function() {
+
+		      	sLoader.fadeOut(); 
+		      	$('#message-warning').html("Something went wrong. Please try again.");
+		         $('#message-warning').fadeIn();
+
+		      }
+
+	      });     		
+  		}
+
+	});
+	
+		/*_________*/
+		// List of sentences
+	var _CONTENT = [
+		"Time Left For Climate Change To Become Irreversible", 
+		"Time To Take Action",
+	];
+
+	// Current sentence being processed
+	var _PART = 0;
+
+	// Character number of the current sentence being processed 
+	var _PART_INDEX = 0;
+
+	// Holds the handle returned from setInterval
+	var _INTERVAL_VAL;
+
+	// Element that holds the text
+	var _ELEMENT = document.querySelector("#text");
+
+	// Cursor element 
+	var _CURSOR = document.querySelector("#cursor");
+
+	// Implements typing effect
+	function Type() { 
+		// Get substring with 1 characater added
+		var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+		_ELEMENT.innerHTML = text;
+		_PART_INDEX++;
+
+		// If full sentence has been displayed then start to delete the sentence after some time
+		if(text === _CONTENT[_PART]) {
+			// Hide the cursor
+			_CURSOR.style.display = 'none';
+
+			clearInterval(_INTERVAL_VAL);
+			setTimeout(function() {
+				_INTERVAL_VAL = setInterval(Delete, 50);
+			}, 1000);
+		}
+	}
+
+		// Implements deleting effect
+	function Delete() {
+		// Get substring with 1 characater deleted
+		var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+		_ELEMENT.innerHTML = text;
+		_PART_INDEX--;
+
+		// If sentence has been deleted then start to display the next sentence
+		if(text === '') {
+			clearInterval(_INTERVAL_VAL);
+
+			// If current sentence was last then display the first one, else move to the next
+			if(_PART == (_CONTENT.length - 1))
+				_PART = 0;
+			else
+				_PART++;
+			
+			_PART_INDEX = 0;
+
+			// Start to display the next sentence after some time
+			setTimeout(function() {
+				_CURSOR.style.display = 'inline-block';
+				_INTERVAL_VAL = setInterval(Type, 100);
+			}, 200);
+		}
+	}
+
+	// Start the typing effect on load
+	_INTERVAL_VAL = setInterval(Type, 100);
+		/*_________*/
+
+	/*----------------------------------------------------*/
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2025/10/09';
+
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+
+   		$(this).html(event.strftime('<div class=\"half\">' +
+   											 '<span>%D <sup>days</sup></span>' + 
+   										 	 '<span>%H <sup>hours</sup></span>' + 
+   										 	 '</div>' +
+   										 	 '<div class=\"half\">' +
+   										 	 '<span>%M <sup>mins</sup></span>' +
+   										 	 '<span>%S <sup>secs</sup></span>' +
+   										 	 '</div>'));
+
+   });     
+ 
+
+})(jQuery);
